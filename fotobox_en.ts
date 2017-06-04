4bd66b4db38c2c232604052fe5a5e6423bf1e0ef
Thomas Kais <tomikais@t-online.de>
2017-06-04 12:57:37 +0200

    add translation files

---
+++ fotobox_en.ts
@@ -0,0 +1,35 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>FotoBox</name>
+    <message>
+        <location filename="fotobox.cpp" line="125"/>
+        <source>Error: Taking a photo isn&apos;t working correctly! Please call the Fotobox owner.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="fotobox.cpp" line="140"/>
+        <source>Couldn&apos;t load the image.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="14"/>
+        <source>FotoBox by Thomas Kais ©</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="46"/>
+        <source>Bild aufnehmen</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="74"/>
+        <source>Beenden</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
