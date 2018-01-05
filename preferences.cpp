54ca21b25abad706a2f372d24b36ffa4d97c1318
Thomas Kais <tomikais@t-online.de>
2018-01-05 15:43:29 +0100

    store settings

--- preferences.cpp
+++ preferences.cpp
@@ -8,15 +8,35 @@
 #include "preferences.h"
 #include "ui_preferences.h"
 
+
 Preferences::Preferences(QWidget *parent) : QDialog(parent),
   m_ui(new Ui::Preferences),
-  m_settings()
+  m_settings(QSettings::IniFormat, QSettings::UserScope, qApp->organizationName(), qApp->applicationName(), this)
 {
   //setup UI
   m_ui->setupUi(this);
+
+  //connect button
+  connect(m_ui->btnOk, &QPushButton::clicked, this, &Preferences::close);
 }
+
 
 Preferences::~Preferences()
 {
   delete m_ui;
+}
+
+
+auto Preferences::closeEvent(QCloseEvent *event) -> void 
+{
+  //GENERAL
+  m_settings.setValue(m_ui->chbButtons->objectName(), m_ui->chbButtons->checkState());
+  
+  //BUZZER
+
+  //CAMERA
+
+
+  //default close request
+  QDialog::closeEvent(event);
 }
--- preferences.h
+++ preferences.h
@@ -13,15 +13,38 @@ class Preferences;
 }
 
 
+/*!
+* \brief The Preferences class
+* Main class to change and store the application settings
+*/
 class Preferences : public QDialog
 {
   Q_OBJECT
 
+
 public:
+  /*!
+  * \brief Preferences constructor
+  * \param parent QWidget
+  */
   explicit Preferences(QWidget *parent = nullptr);
+
+  /*!
+  * \brief FotoBox destructor
+  */
   ~Preferences();
 
+
 private:
+  /*!
+  * \brief override void QWidget::closeEvent(QCloseEvent *event)
+  * \details save application settings to INI file
+  */
+  auto closeEvent(QCloseEvent *event) -> void override;
+
+  //User Interface
   Ui::Preferences *m_ui;
+
+  //Store and read settings (INI file)
   QSettings m_settings;
 };
