6ab90ac9615cd37cfb5361d21a111c5d4415aa24
Thomas Kais <tomikais@t-online.de>
2018-02-26 15:02:26 +0100

    translation

--- translation_de.ts
+++ translation_de.ts
@@ -4,35 +4,35 @@
 <context>
     <name>FotoBox</name>
     <message>
-        <location filename="fotobox.cpp" line="106"/>
+        <location filename="fotobox.cpp" line="95"/>
         <source>Please install &apos;gphoto2&apos; on &lt;br&gt;&lt;br&gt;Raspberry Pi: &lt;a href=&apos;https://github.com/gonzalo/gphoto2-updater&apos;&gt;gphoto2-updater&lt;/a&gt;&lt;br&gt;macOS: &lt;a href=&apos;https://brew.sh&gt;Homebrew/&apos;&gt;Homebrew&lt;/a&gt;</source>
         <translation>Bitte installiere &apos;gphoto2&apos; auf deinem &lt;br&gt;&lt;br&gt;Raspberry Pi: &lt;a href=&apos;https://github.com/gonzalo/gphoto2-updater&apos;&gt;gphoto2-updater&lt;/a&gt;&lt;br&gt;macOS: &lt;a href=&apos;https://brew.sh&gt;Homebrew/&apos;&gt;Homebrew&lt;/a&gt;</translation>
     </message>
     <message>
-        <location filename="fotobox.cpp" line="138"/>
+        <location filename="fotobox.cpp" line="130"/>
         <source>Error: Taking a photo isn&apos;t working correctly! Please call the Fotobox owner.</source>
         <translation>Fehler: Aufnehmen des Fotos funktioniert nicht! Bitte informieren sie den Fotobox Besitzer.</translation>
     </message>
     <message>
-        <location filename="fotobox.cpp" line="153"/>
+        <location filename="fotobox.cpp" line="122"/>
         <source>Couldn&apos;t load the image.</source>
         <translation>Kann das Bild nicht laden.</translation>
     </message>
 </context>
 <context>
-    <name>MainWindow</name>
+    <name>Fotobox</name>
     <message>
-        <location filename="mainwindow.ui" line="14"/>
+        <location filename="fotobox.ui" line="14"/>
         <source>FotoBox by Thomas Kais ©</source>
         <translation>Fotobox von Thomas Kais ©</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="51"/>
+        <location filename="fotobox.ui" line="46"/>
         <source>take picture</source>
         <translation>Bild aufnehmen</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="79"/>
+        <location filename="fotobox.ui" line="68"/>
         <source>exit</source>
         <translation>Beenden</translation>
     </message>
@@ -40,85 +40,85 @@
 <context>
     <name>Preferences</name>
     <message>
-        <location filename="preferences.ui" line="24"/>
+        <location filename="preferences.ui" line="93"/>
         <source>Choose Color</source>
         <translation>Wähle Farbe</translation>
     </message>
     <message>
-        <location filename="preferences.ui" line="31"/>
+        <location filename="preferences.ui" line="117"/>
+        <source> milliseconds</source>
+        <translation> Millisekunden</translation>
+    </message>
+    <message>
+        <location filename="preferences.ui" line="130"/>
+        <source>query interval</source>
+        <translation>Abfrageintervall</translation>
+    </message>
+    <message>
+        <location filename="preferences.ui" line="171"/>
         <source>output PIN</source>
         <translation>Output PIN</translation>
     </message>
     <message>
-        <location filename="preferences.ui" line="44"/>
+        <location filename="preferences.ui" line="164"/>
         <source>Camera</source>
         <translation>Kamera</translation>
     </message>
     <message>
-        <location filename="preferences.ui" line="64"/>
+        <location filename="preferences.ui" line="191"/>
         <source>buttons</source>
         <translation>Buttons</translation>
     </message>
     <message>
-        <location filename="preferences.ui" line="71"/>
+        <location filename="preferences.ui" line="137"/>
         <source>timeout value</source>
         <translation>Timeout-Wert</translation>
     </message>
     <message>
-        <location filename="preferences.ui" line="78"/>
+        <location filename="preferences.ui" line="79"/>
         <source>(enable/disable)</source>
         <translation>(einschalten/ausschalten)</translation>
     </message>
     <message>
-        <location filename="preferences.ui" line="85"/>
-        <source>--capture-image-and-download --keep --filename preview.jpg --set-config /main/settings/capturetarget=1 --force-overwrite</source>
-        <translation></translation>
-    </message>
-    <message>
-        <location filename="preferences.ui" line="95"/>
+        <location filename="preferences.ui" line="107"/>
         <source>background color</source>
         <translation>Hintergrund Farbe</translation>
     </message>
     <message>
-        <location filename="preferences.ui" line="108"/>
+        <location filename="preferences.ui" line="204"/>
         <source>General</source>
         <translation>Allgemein</translation>
     </message>
     <message>
-        <location filename="preferences.ui" line="115"/>
-        <location filename="preferences.cpp" line="79"/>
+        <location filename="preferences.ui" line="72"/>
+        <location filename="preferences.cpp" line="86"/>
         <source> seconds</source>
         <translation> sekunden</translation>
     </message>
     <message>
-        <location filename="preferences.ui" line="131"/>
+        <location filename="preferences.ui" line="184"/>
         <source>Buzzer</source>
         <translation>Buzzer</translation>
     </message>
     <message>
-        <location filename="preferences.ui" line="138"/>
+        <location filename="preferences.ui" line="144"/>
         <source>GPhoto2 argument line</source>
         <translation>GPhoto2 Parameter</translation>
     </message>
     <message>
-        <location filename="preferences.ui" line="164"/>
-        <source>#000000</source>
-        <translation></translation>
-    </message>
-    <message>
-        <location filename="preferences.ui" line="187"/>
+        <location filename="preferences.ui" line="86"/>
         <source>input PIN</source>
         <translation>Input PIN</translation>
     </message>
     <message>
-        <location filename="preferences.cpp" line="79"/>
-        <source>launching Fotobox in </source>
-        <translation>Starte Fotobox in </translation>
+        <location filename="preferences.cpp" line="86"/>
+        <source>launching FotoBox in </source>
+        <translation>FotoBox wird gestartet in </translation>
     </message>
     <message>
-        <location filename="preferences.cpp" line="96"/>
-        <source>Fotobox preferences</source>
-        <translation>Fotobox Einstellungen</translation>
+        <location filename="preferences.cpp" line="103"/>
+        <source>FotoBox preferences</source>
+        <translation>FotoBox preferences</translation>
     </message>
 </context>
 </TS>
--- translation_en.ts
+++ translation_en.ts
@@ -4,35 +4,35 @@
 <context>
     <name>FotoBox</name>
     <message>
-        <location filename="fotobox.cpp" line="106"/>
+        <location filename="fotobox.cpp" line="95"/>
         <source>Please install &apos;gphoto2&apos; on &lt;br&gt;&lt;br&gt;Raspberry Pi: &lt;a href=&apos;https://github.com/gonzalo/gphoto2-updater&apos;&gt;gphoto2-updater&lt;/a&gt;&lt;br&gt;macOS: &lt;a href=&apos;https://brew.sh&gt;Homebrew/&apos;&gt;Homebrew&lt;/a&gt;</source>
         <translation>Please install &apos;gphoto2&apos; on &lt;br&gt;&lt;br&gt;Raspberry Pi: &lt;a href=&apos;https://github.com/gonzalo/gphoto2-updater&apos;&gt;gphoto2-updater&lt;/a&gt;&lt;br&gt;macOS: &lt;a href=&apos;https://brew.sh&gt;Homebrew/&apos;&gt;Homebrew&lt;/a&gt;</translation>
     </message>
     <message>
-        <location filename="fotobox.cpp" line="138"/>
+        <location filename="fotobox.cpp" line="130"/>
         <source>Error: Taking a photo isn&apos;t working correctly! Please call the Fotobox owner.</source>
         <translation>Error: Taking a photo isn&apos;t working correctly! Please call the Fotobox owner.</translation>
     </message>
     <message>
-        <location filename="fotobox.cpp" line="153"/>
+        <location filename="fotobox.cpp" line="122"/>
         <source>Couldn&apos;t load the image.</source>
         <translation>Couldn&apos;t load the image.</translation>
     </message>
 </context>
 <context>
-    <name>MainWindow</name>
+    <name>Fotobox</name>
     <message>
-        <location filename="mainwindow.ui" line="14"/>
+        <location filename="fotobox.ui" line="14"/>
         <source>FotoBox by Thomas Kais ©</source>
         <translation>FotoBox by Thomas Kais ©</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="51"/>
+        <location filename="fotobox.ui" line="46"/>
         <source>take picture</source>
         <translation>take picture</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="79"/>
+        <location filename="fotobox.ui" line="68"/>
         <source>exit</source>
         <translation>exit</translation>
     </message>
@@ -40,85 +40,85 @@
 <context>
     <name>Preferences</name>
     <message>
-        <location filename="preferences.ui" line="24"/>
+        <location filename="preferences.ui" line="93"/>
         <source>Choose Color</source>
         <translation>Choose Color</translation>
     </message>
     <message>
-        <location filename="preferences.ui" line="31"/>
+        <location filename="preferences.ui" line="117"/>
+        <source> milliseconds</source>
+        <translation> milliseconds</translation>
+    </message>
+    <message>
+        <location filename="preferences.ui" line="130"/>
+        <source>query interval</source>
+        <translation>query interval</translation>
+    </message>
+    <message>
+        <location filename="preferences.ui" line="171"/>
         <source>output PIN</source>
         <translation>output PIN</translation>
     </message>
     <message>
-        <location filename="preferences.ui" line="44"/>
+        <location filename="preferences.ui" line="164"/>
         <source>Camera</source>
         <translation>Camera</translation>
     </message>
     <message>
-        <location filename="preferences.ui" line="64"/>
+        <location filename="preferences.ui" line="191"/>
         <source>buttons</source>
         <translation>buttons</translation>
     </message>
     <message>
-        <location filename="preferences.ui" line="71"/>
+        <location filename="preferences.ui" line="137"/>
         <source>timeout value</source>
         <translation>timeout value</translation>
     </message>
     <message>
-        <location filename="preferences.ui" line="78"/>
+        <location filename="preferences.ui" line="79"/>
         <source>(enable/disable)</source>
         <translation>(enable/disable)</translation>
     </message>
     <message>
-        <location filename="preferences.ui" line="85"/>
-        <source>--capture-image-and-download --keep --filename preview.jpg --set-config /main/settings/capturetarget=1 --force-overwrite</source>
-        <translation></translation>
-    </message>
-    <message>
-        <location filename="preferences.ui" line="95"/>
+        <location filename="preferences.ui" line="107"/>
         <source>background color</source>
         <translation>background color</translation>
     </message>
     <message>
-        <location filename="preferences.ui" line="108"/>
+        <location filename="preferences.ui" line="204"/>
         <source>General</source>
         <translation>General</translation>
     </message>
     <message>
-        <location filename="preferences.ui" line="115"/>
-        <location filename="preferences.cpp" line="79"/>
+        <location filename="preferences.ui" line="72"/>
+        <location filename="preferences.cpp" line="86"/>
         <source> seconds</source>
         <translation> seconds</translation>
     </message>
     <message>
-        <location filename="preferences.ui" line="131"/>
+        <location filename="preferences.ui" line="184"/>
         <source>Buzzer</source>
         <translation>Buzzer</translation>
     </message>
     <message>
-        <location filename="preferences.ui" line="138"/>
+        <location filename="preferences.ui" line="144"/>
         <source>GPhoto2 argument line</source>
         <translation>GPhoto2 argument line</translation>
     </message>
     <message>
-        <location filename="preferences.ui" line="164"/>
-        <source>#000000</source>
-        <translation></translation>
-    </message>
-    <message>
-        <location filename="preferences.ui" line="187"/>
+        <location filename="preferences.ui" line="86"/>
         <source>input PIN</source>
         <translation>input PIN</translation>
     </message>
     <message>
-        <location filename="preferences.cpp" line="79"/>
-        <source>launching Fotobox in </source>
-        <translation>launching Fotobox in </translation>
+        <location filename="preferences.cpp" line="86"/>
+        <source>launching FotoBox in </source>
+        <translation>launching FotoBox in </translation>
     </message>
     <message>
-        <location filename="preferences.cpp" line="96"/>
-        <source>Fotobox preferences</source>
-        <translation>Fotobox preferences</translation>
+        <location filename="preferences.cpp" line="103"/>
+        <source>FotoBox preferences</source>
+        <translation>Fotobox Einstellungen</translation>
     </message>
 </context>
 </TS>
