a10143d3ae0933b061d640ea1912e64907caeddd
Thomas Kais <thomas.kais@heidelberg.com>
2018-06-08 15:27:35 +0200

    restructure project files

--- fotobox.pro
+++ fotobox.pro
@@ -7,58 +7,64 @@
 # file 'LICENSE', which is part of this source code package.
 #------------------------------------------------------------
 
-greaterThan(QT_MAJOR_VERSION, 5)
-QT              += core gui widgets
-
-CONFIG          += c++11
-
-TARGET           = fotobox
-target.path      = /home/pi
-INSTALLS        += target
-
-TEMPLATE         = app
-
-SOURCES         += main.cpp \
-                   fotobox.cpp \
-                   buzzer.cpp \
-                   camera.cpp \
-                   preferences.cpp \
-                   preferenceprovider.cpp
-
-HEADERS         += fotobox.h \
-                   buzzer.h \
-                   camera.h \
-                   preferences.h \
-                   preferenceprovider.h
-
-FORMS            = fotobox.ui \
-                   preferences.ui
-
-RESOURCES       += fotobox.qrc
-TRANSLATIONS    += translation_en.ts \
-                   translation_de.ts
-
-OTHER_FILES     += README.md \
-                   LICENSE
-
-linux{
-    # Speed-Up compiling time with ccache (apt-get install ccache)
-    QMAKE_CXX        = ccache g++
+contains(QT_MAJOR_VERSION, 5):lessThan(QT_MINOR_VERSION, 7)
+{
+  error("Your Qt version is too old (required >=5.7.0)")
 }
-# add make argument '-j4'
 
+QT            += core gui widgets
 
-# wiringPi libs
-linux {
-    # DEVICE: Raspberry Pi (wiringPi available)
-    contains(QMAKE_HOST.arch, arm.*): {
-        # debug or release build
-        debug {
-            LIBS            += -lwiringPiDev
-        } release {
-            LIBS            += -lwiringPi
-        }
-    } else {
-      #DEVICE: other (no wiringPi available)
+CONFIG        += c++11
+
+TARGET         = FotoBox
+target.path    = /home/pi
+INSTALLS      += target
+
+TEMPLATE       = app
+
+SOURCES       += src/main.cpp \
+                 src/fotobox.cpp \
+                 src/buzzer.cpp \
+                 src/camera.cpp \
+                 src/preferences.cpp \
+                 src/preferenceprovider.cpp
+
+INCLUDEPATH   += src
+HEADERS       += src/fotobox.h \
+                 src/buzzer.h \
+                 src/camera.h \
+                 src/preferences.h \
+                 src/preferenceprovider.h
+
+FORMS          = forms/fotobox.ui \
+                 forms/preferences.ui
+
+RESOURCES     += resources/qresource.qrc
+TRANSLATIONS  += resources/translation_en.ts \
+                 resources/translation_de.ts
+
+OTHER_FILES   += README.md \
+                 LICENSE
+
+# Speed-Up compiling time with ccache (apt-get install ccache)
+linux
+{
+  QMAKE_CXX = ccache g++
+}
+
+# wiringPi framework
+linux
+{
+  # DEVICE: Raspberry Pi (wiringPi available)
+  contains(QMAKE_HOST.arch, arm.*):
+  {
+    debug
+    {
+      LIBS += -lwiringPiDev
     }
+    release
+    {
+      LIBS += -lwiringPi
+    }
+  }
 }
--- resources/translation_en.ts
+++ resources/translation_en.ts
@@ -4,12 +4,12 @@
 <context>
     <name>FotoBox</name>
     <message>
-        <location filename="fotobox.cpp" line="155"/>
+        <location filename="../src/fotobox.cpp" line="183"/>
         <source>Error: Taking a photo isn&apos;t working correctly! Please call the FotoBox owner.</source>
         <translation>Error: Taking a photo isn&apos;t working correctly! Please call the FotoBox owner.</translation>
     </message>
     <message>
-        <location filename="fotobox.cpp" line="192"/>
+        <location filename="../src/fotobox.cpp" line="224"/>
         <source>Couldn&apos;t load the photo.</source>
         <translation>Couldn&apos;t load the photo.</translation>
     </message>
@@ -17,22 +17,22 @@
 <context>
     <name>FotoBoxDialog</name>
     <message>
-        <location filename="fotobox.ui" line="15"/>
+        <location filename="../forms/fotobox.ui" line="15"/>
         <source>FotoBox (Copyright Thomas Kais)</source>
         <translation>FotoBox (Copyright Thomas Kais)</translation>
     </message>
     <message>
-        <location filename="fotobox.ui" line="76"/>
+        <location filename="../forms/fotobox.ui" line="76"/>
         <source>take picture</source>
         <translation>take picture</translation>
     </message>
     <message>
-        <location filename="fotobox.ui" line="46"/>
+        <location filename="../forms/fotobox.ui" line="46"/>
         <source>preferences</source>
         <translation>preferences</translation>
     </message>
     <message>
-        <location filename="fotobox.ui" line="31"/>
+        <location filename="../forms/fotobox.ui" line="31"/>
         <source>exit</source>
         <translation>exit</translation>
     </message>
@@ -40,38 +40,38 @@
 <context>
     <name>Preferences</name>
     <message>
-        <location filename="preferences.cpp" line="114"/>
+        <location filename="../src/preferences.cpp" line="112"/>
         <source> seconds</source>
         <translation> seconds</translation>
     </message>
     <message>
-        <location filename="preferences.cpp" line="114"/>
+        <location filename="../src/preferences.cpp" line="112"/>
         <source>launching FotoBox in </source>
         <translation>launching FotoBox in </translation>
     </message>
     <message>
-        <location filename="preferences.cpp" line="133"/>
+        <location filename="../src/preferences.cpp" line="130"/>
         <source>FotoBox preferences</source>
         <translation>FotoBox Einstellungen</translation>
     </message>
     <message>
-        <location filename="preferences.cpp" line="47"/>
+        <location filename="../src/preferences.cpp" line="44"/>
         <source>choose directory</source>
         <translation>choose directory</translation>
     </message>
     <message>
-        <location filename="preferences.cpp" line="273"/>
-        <location filename="preferences.cpp" line="294"/>
+        <location filename="../src/preferences.cpp" line="270"/>
+        <location filename="../src/preferences.cpp" line="291"/>
         <source>&apos;%1&apos; is missing! Get it </source>
         <translation>&apos;%1&apos; is missing! Get it </translation>
     </message>
     <message>
-        <location filename="preferences.cpp" line="283"/>
+        <location filename="../src/preferences.cpp" line="280"/>
         <source>*** no camera detected ***</source>
         <translation>*** no camera detected ***</translation>
     </message>
     <message>
-        <location filename="preferences.cpp" line="304"/>
+        <location filename="../src/preferences.cpp" line="301"/>
         <source>&apos; is missing!</source>
         <translation>&apos; is missing!</translation>
     </message>
@@ -79,102 +79,102 @@
 <context>
     <name>PreferencesDialog</name>
     <message>
-        <location filename="preferences.ui" line="146"/>
+        <location filename="../forms/preferences.ui" line="146"/>
         <source>input PIN</source>
         <translation>input PIN</translation>
     </message>
     <message>
-        <location filename="preferences.ui" line="166"/>
+        <location filename="../forms/preferences.ui" line="166"/>
         <source>background color</source>
         <translation>background color</translation>
     </message>
     <message>
-        <location filename="preferences.ui" line="226"/>
+        <location filename="../forms/preferences.ui" line="226"/>
         <source> seconds</source>
         <translation> seconds</translation>
     </message>
     <message>
-        <location filename="preferences.ui" line="209"/>
+        <location filename="../forms/preferences.ui" line="209"/>
         <source>(enable/disable)</source>
         <translation>(enable/disable)</translation>
     </message>
     <message>
-        <location filename="preferences.ui" line="112"/>
+        <location filename="../forms/preferences.ui" line="112"/>
         <source>query interval</source>
         <translation>query interval</translation>
     </message>
     <message>
-        <location filename="preferences.ui" line="156"/>
+        <location filename="../forms/preferences.ui" line="156"/>
         <source>photo folder</source>
         <translation>photo folder</translation>
     </message>
     <message>
-        <location filename="preferences.ui" line="176"/>
+        <location filename="../forms/preferences.ui" line="176"/>
         <source>photo name</source>
         <translation>photo name</translation>
     </message>
     <message>
-        <location filename="preferences.ui" line="199"/>
+        <location filename="../forms/preferences.ui" line="199"/>
         <source>buttons</source>
         <translation>buttons</translation>
     </message>
     <message>
-        <location filename="preferences.ui" line="233"/>
+        <location filename="../forms/preferences.ui" line="233"/>
         <source>Choose Directory</source>
         <translation>Choose Directory</translation>
     </message>
     <message>
-        <location filename="preferences.ui" line="250"/>
+        <location filename="../forms/preferences.ui" line="250"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;a href=&quot;http://wiringpi.com/wp-content/uploads/2013/03/pins.pdf&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;Pins 0 through 6&lt;/span&gt;&lt;/a&gt; (BCM_GPIO 17, 18, 21, 22, 23, 24, 25 respectively, substitute 27 for 21 on a Rev. 2 board): These are safe to use at any time and can be set to input or output with or without the internal pull-up or pull-down resistors enabled.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
         <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;a href=&quot;http://wiringpi.com/wp-content/uploads/2013/03/pins.pdf&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;Pins 0 through 6&lt;/span&gt;&lt;/a&gt; (BCM_GPIO 17, 18, 21, 22, 23, 24, 25 respectively, substitute 27 for 21 on a Rev. 2 board): These are safe to use at any time and can be set to input or output with or without the internal pull-up or pull-down resistors enabled.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
-        <location filename="preferences.ui" line="279"/>
+        <location filename="../forms/preferences.ui" line="279"/>
         <source>FotoBox</source>
         <translation>FotoBox</translation>
     </message>
     <message>
-        <location filename="preferences.ui" line="286"/>
+        <location filename="../forms/preferences.ui" line="286"/>
         <source>Choose Color</source>
         <translation>Choose Color</translation>
     </message>
     <message>
-        <location filename="preferences.ui" line="50"/>
+        <location filename="../forms/preferences.ui" line="50"/>
         <source>timeout value</source>
         <translation>timeout value</translation>
     </message>
     <message>
-        <location filename="preferences.ui" line="89"/>
+        <location filename="../forms/preferences.ui" line="89"/>
         <source>Camera</source>
         <translation>Camera</translation>
     </message>
     <message>
-        <location filename="preferences.ui" line="129"/>
+        <location filename="../forms/preferences.ui" line="129"/>
         <source>output PIN</source>
         <translation>output PIN</translation>
     </message>
     <message>
-        <location filename="preferences.ui" line="73"/>
+        <location filename="../forms/preferences.ui" line="73"/>
         <source>camera mode</source>
         <translation>camera mode</translation>
     </message>
     <message>
-        <location filename="preferences.ui" line="99"/>
+        <location filename="../forms/preferences.ui" line="99"/>
         <source> milliseconds</source>
         <translation> milliseconds</translation>
     </message>
     <message>
-        <location filename="preferences.ui" line="192"/>
+        <location filename="../forms/preferences.ui" line="192"/>
         <source>Buzzer</source>
         <translation>Buzzer</translation>
     </message>
     <message>
-        <location filename="preferences.ui" line="60"/>
+        <location filename="../forms/preferences.ui" line="60"/>
         <source>argument line</source>
         <translation>argument line</translation>
     </message>
     <message>
-        <location filename="preferences.ui" line="263"/>
+        <location filename="../forms/preferences.ui" line="263"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;If you have a &lt;span style=&quot; font-style:italic;&quot;&gt;DSLR camera&lt;/span&gt;, choose &amp;quot;&lt;span style=&quot; font-weight:600;&quot;&gt;gphoto2&lt;/span&gt;&amp;quot; mode. If you want to use the &lt;span style=&quot; font-style:italic;&quot;&gt;Raspberry Pi Camera Module&lt;/span&gt;, choose &amp;quot;&lt;span style=&quot; font-weight:600;&quot;&gt;raspistill&lt;/span&gt;&amp;quot; mode.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
         <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;If you have a &lt;span style=&quot; font-style:italic;&quot;&gt;DSLR camera&lt;/span&gt;, choose &amp;quot;&lt;span style=&quot; font-weight:600;&quot;&gt;gphoto2&lt;/span&gt;&amp;quot; mode. If you want to use the &lt;span style=&quot; font-style:italic;&quot;&gt;Raspberry Pi Camera Module&lt;/span&gt;, choose &amp;quot;&lt;span style=&quot; font-weight:600;&quot;&gt;raspistill&lt;/span&gt;&amp;quot; mode.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
