e6779ef35d2fc1133bf23dbb2c11c5fcc898a7bb
Thomas Kais <tomikais@t-online.de>
2019-07-28 13:24:56 +0200

    fix clazy finding

--- source/preferences.cpp
+++ source/preferences.cpp
@@ -312,7 +312,7 @@ void Preferences::verifyApplication(const QString &i_name)
     }
 
     //Other Applications
-    auto message = tr("'%1' is missing%2").arg(i_name, QStringLiteral(""));
+    auto message = tr("'%1' is missing%2").arg(i_name, QLatin1String(""));
     applicationAvailable(i_name, message);
 }
 
