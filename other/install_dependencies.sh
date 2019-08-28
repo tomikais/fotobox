decb14e83ead000d2ba8ce61beca7e97bdadd3c4
Thomas Kais <tomikais@t-online.de>
2019-08-28 10:19:24 +0200

    Linux installation script refactoring

--- other/install_dependencies.sh
+++ other/install_dependencies.sh
@@ -8,24 +8,52 @@ set -Eeuxo pipefail
 # file 'COPYING', which is part of this source code package.
 #------------------------------------------------------------
 
-echo "Prepare system for 'FotoBox'..."
+
+if [ "$(whoami)" != "root" ]; then
+    echo "Sorry, this script must be executed with 'sudo' or as root"
+    exit 1
+fi
+
+
+echo "Prepare your operating system for 'FotoBox'"
 echo "estimated running time: 15-30 minutes"
 echo ""
-read -r -p "Are you sure? [y/N] " response
+read -r -p "Start with the installation? [y/N] " response
 response=${response,,}    # tolower
-if [[ "$response" =~ ^(yes|y)$ ]]; then
-  echo "install WiringPi, Qt and XScreenSaver"
-  sudo apt-get update -qq
-  sudo apt-get install -qq \
-    wiringpi     \
-    qt5-default  \
-    xscreensaver
-
-  xhost +localhost
-
-  echo "install gphoto2"
-  sudo apt-get purge gphoto2 libgphoto2-6
-  curl --silent https://raw.githubusercontent.com/gonzalo/gphoto2-updater/master/gphoto2-updater.sh
-  chmod +x gphoto2-updater.sh
-  sudo ./gphoto2-updater.sh --stable
+if [[ "$response" =~ ^(no|n)$ ]]; then
+    exit 0;
 fi
+
+
+echo
+echo "-----------------------"
+echo "Installing dependencies"
+echo "-----------------------"
+echo
+apt-get update --quiet
+apt-get purge --yes \
+            gphoto2 \
+            libgphoto2-6
+apt-get install --yes \
+            wget \
+            build-essential \
+            git \
+            qt5-default
+
+echo
+echo "-----------------------------"
+echo "Installing libgphoto2/gphoto2"
+echo "-----------------------------"
+echo
+wget --quiet https://raw.githubusercontent.com/gonzalo/gphoto2-updater/master/gphoto2-updater.sh
+chmod +x gphoto2-updater.sh
+./gphoto2-updater.sh --stable
+rm --force gphoto2-updater.sh
+
+
+echo
+echo "---------------------------"
+echo "! INSTALLATION SUCCESSFUL !"
+echo "!      enjoy FotoBox      !"
+echo "---------------------------"
+echo
