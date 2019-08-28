49e461ba2ff6887b4b20ceb65226495334e1d125
Thomas Kais <tomikais@t-online.de>
2019-08-28 11:23:15 +0200

    install script: reduce bash output; use a temporary directory;

--- other/install_dependencies.sh
+++ other/install_dependencies.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -Eeuxo pipefail
+set -Eeuo pipefail
 
 #------------------------------------------------------------
 # Copyright (c) 2019 Thomas Kais
@@ -9,15 +9,21 @@ set -Eeuxo pipefail
 #------------------------------------------------------------
 
 
+
 if [ "$(whoami)" != "root" ]; then
     echo "Sorry, this script must be executed with 'sudo' or as root"
     exit 1
 fi
 
 
+
+echo
+echo "-------------------------------------------"
 echo "Prepare your operating system for 'FotoBox'"
 echo "estimated running time: 15-30 minutes"
-echo ""
+echo "-------------------------------------------"
+echo
+
 read -r -p "Start with the installation? [y/N] " response
 response=${response,,}    # tolower
 if [[ "$response" =~ ^(no|n)$ ]]; then
@@ -25,35 +31,70 @@ if [[ "$response" =~ ^(no|n)$ ]]; then
 fi
 
 
+
+echo
+echo "-------------------------"
+echo "Creating temporary folder"
+echo "-------------------------"
+echo
+
+# the temp directory
+TMP_DIR="$(mktemp --directory)"
+# check if tmp dir was created
+if [[ ! "$TMP_DIR" || ! -d "$TMP_DIR" ]]; then
+    echo "Could not create temporary directory"
+    exit 1
+fi
+# deletes the temp directory
+function cleanup {
+    echo
+    echo "-------------------"
+    echo "Removing temp files"
+    echo "-------------------"
+    echo
+
+    rm --force --recursive "$TMP_DIR"
+}
+# register the cleanup function to be called on the EXIT signal
+trap cleanup EXIT
+pushd "$TMP_DIR"
+
+
+
 echo
 echo "-----------------------"
 echo "Installing dependencies"
 echo "-----------------------"
 echo
+
 apt-get update --quiet
-apt-get purge --yes \
+apt-get purge --quiet --yes \
             gphoto2 \
             libgphoto2-6
-apt-get install --yes \
+apt-get install --quiet --yes \
             wget \
             build-essential \
             git \
             qt5-default
+
+
 
 echo
 echo "-----------------------------"
 echo "Installing libgphoto2/gphoto2"
 echo "-----------------------------"
 echo
+
 wget --quiet https://raw.githubusercontent.com/gonzalo/gphoto2-updater/master/gphoto2-updater.sh
 chmod +x gphoto2-updater.sh
 ./gphoto2-updater.sh --stable
-rm --force gphoto2-updater.sh
+popd
+
 
 
 echo
-echo "---------------------------"
+echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!"
 echo "! INSTALLATION SUCCESSFUL !"
 echo "!      enjoy FotoBox      !"
-echo "---------------------------"
+echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!"
 echo
